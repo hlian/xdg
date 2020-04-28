f3b9fee4bee20316547ea71bcb4ae93030779a8c
Hao Lian <hi@haolian.org>
2020-04-28 14:01:59 -0400

    zsh

---
+++ zsh/runme.zsh
@@ -0,0 +1,5 @@
+git clone git@github.com:hlian/prezto.git "${ZDOTDIR:-$HOME}"/.zprezto
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
